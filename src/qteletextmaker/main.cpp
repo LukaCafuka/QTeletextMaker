a7e93f463a4cabae10b37809cb2ac92bd9a9aeef
Gavin MacGregor <gkthemac@gmail.com>
2025-12-02 11:34:16 +0000

    Tag version 0.8.2-beta

--- src/qteletextmaker/main.cpp
+++ src/qteletextmaker/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	QApplication::setApplicationDisplayName(QApplication::applicationName());
 	QApplication::setOrganizationName("gkmac.co.uk");
 	QApplication::setOrganizationDomain("gkmac.co.uk");
-	QApplication::setApplicationVersion("0.8.1-beta");
+	QApplication::setApplicationVersion("0.8.2-beta");
 	QCommandLineParser parser;
 	parser.setApplicationDescription(QApplication::applicationName());
 	parser.addHelpOption();
